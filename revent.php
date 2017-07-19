82c622c735519df23826ff7ca8f47202c97fcdbd
systopia <endres@systopia.de>
2017-07-19 11:22:46 +0200

    minor adjustments

--- revent.php
+++ revent.php
@@ -81,7 +81,7 @@ function revent_civicrm_enable() {
   _revent_civix_civicrm_enable();
 
   require_once 'CRM/Revent/CustomData.php';
-  $customData = new CRM_Revent_CustomData('de.systopia.contract');
+  $customData = new CRM_Revent_CustomData('de.systopia.revent');
   $customData->syncOptionGroup(__DIR__ . '/resources/option_group_remote_registration_fields.json');
   $customData->syncCustomGroup(__DIR__ . '/resources/custom_group_remote_event_connection.json');
   $customData->syncCustomGroup(__DIR__ . '/resources/custom_group_remote_event_registration.json');
