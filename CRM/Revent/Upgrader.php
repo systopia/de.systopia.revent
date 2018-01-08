06de62d70e339f199b593cbe971ca5a61c7f74b5
systopia <endres@systopia.de>
2018-01-08 13:47:47 +0100

    fixed HBS-6326

--- CRM/Revent/Upgrader.php
+++ CRM/Revent/Upgrader.php
@@ -45,4 +45,21 @@ class CRM_Revent_Upgrader extends CRM_Revent_Upgrader_Base {
     return TRUE;
   }
 
+  /**
+   * Extend length of civicrm_value_remote_event_registration.registration_fields to varchar(2048)
+   *
+   * @return TRUE on success
+   * @throws Exception
+   */
+  public function upgrade_1000() {
+    $this->ctx->log->info('Applying update 1.0.beta2 - changing size of the registration_fields column');
+    CRM_Core_DAO::executeQuery('ALTER TABLE civicrm_value_remote_event_registration MODIFY registration_fields VARCHAR(2048);');
+    try {
+      CRM_Core_DAO::executeQuery('ALTER TABLE log_civicrm_value_remote_event_registration MODIFY registration_fields VARCHAR(2048);');
+    } catch (Exception $e) {
+      // most likely cause: logging not enabled - that's fine then
+    }
+    return TRUE;
+  }
+
 }
